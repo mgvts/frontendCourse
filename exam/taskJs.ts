d55402cd4466d0615f3ca6cd193ede90f11cba5c
mgvts <89970331+mgvts@users.noreply.github.com>
2023-12-25 02:01:56 +0300

    change exam taskJs.ts

--- exam/taskJs.ts
+++ exam/taskJs.ts
@@ -3,40 +3,63 @@
 // В объекте в качестве значений могут присутствовать только примитивы, массивы и объекты
 //  (функций, Date object, классов и т.д. нет)
 
-type kkey = string | symbol | number
+type kkey =
+    | string
+    | symbol
+    | number
 
-type primitive = number
+type primitive =
+    | string
     | null
     | undefined
-    | string
-    | []
+    | kkey
 
-type oobj = {[key: kkey]: primitive};
+
+type oobj = {[key: kkey]: primitive | primitive[] | oobj};
 
 function deepClone(obj: oobj): oobj {
+    function cloneVal(v) {
+        let value = v
+        if (Array.isArray(value)) {
+            return value.map((e) => cloneVal(e))
+        }
+
+        if (typeof value == 'object'){
+            if (value == null){
+                return  null
+            }
+            return deepClone(value as oobj)
+        }
+        return value
+    }
     // JSON.parse(JSON.stringify(obj));
     // https://developer.mozilla.org/en-US/docs/Web/API/structuredClone
-    let clonned = {} as {[key: kkey]: primitive}
+    let clonned = {} as oobj
     for (let key in obj) {
-        clonned[key] = obj[key]
+        clonned[key] = cloneVal(obj[key])
     }
     return clonned;
 }
 
-const obj1 = {
+let obj1 = {
     a:1,
     b:'str',
-    l: ['liist',
-        {'someListObj': 42}],
+    l: ['liist', {}],
+    'null': null,
+    'und': undefined,
     o: {
         1:'a',
         2:{
             11:'deeep'
         }
     }
-}
+} as oobj
 
 let cl = deepClone(obj1)
 cl['some'] = 321
+obj1['l'][1]['a'] = '123'
 console.log(cl)
 console.log(obj1)
+console.log((cl['l'] as []).length)
+console.log((obj1['l'] as []).length)
+
